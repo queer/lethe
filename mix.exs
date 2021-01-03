48566329ff53a3cfab9398e1ca1092e14258b7f7
amy null <null@amy.gg>
2021-01-02 22:24:14 -0500

    0.2.0 - operators etc

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Lethe.MixProject do
   def project do
     [
       app: :lethe,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
