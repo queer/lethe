3869a73462c03a92c7c7854d6e2928cbdae65322
amy null <null@amy.gg>
2021-01-04 15:14:00 -0500

    0.3.1 - Fix a bug

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Lethe.MixProject do
   def project do
     [
       app: :lethe,
-      version: "0.3.0",
+      version: "0.3.1",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
